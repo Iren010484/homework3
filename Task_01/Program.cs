7d0160f2f63cefc6be791ea373539c953019b3b3
Iren <Iren010484@mail.ru>
2023-06-21 11:02:40 +0500

    Решение первой задачи

---
+++ Task_01/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+23432 -> да
+14212 -> нет
+12821 -> да*/
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine()); 
+    return number;
+}
+
+bool Validate(int numb)
+{
+    if (numb > 99999 || numb < 10000) 
+        {   
+            Console.WriteLine("Число не пятизначное");
+            return false;
+        }
+    return true;
+}
+
+bool Polydrome(int number)
+{
+    if (number %10 == number /10000) 
+    {
+        if (number %100 /10 == number %10000 /1000)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int digit = ReadInt("Введите число");
+if (Validate(digit))
+{
+   if (Polydrome(digit))
+    {
+        Console.WriteLine($" Число {digit} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($" Число {digit} - не является палиндромом");
+    } 
+}
